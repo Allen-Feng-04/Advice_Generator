d26c61f504970429cd03c0eacbbbbbad7fd09962
Allen-Feng-04 <azf2011@stern.nyu.edu>
2024-10-27 01:14:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ function App() {
     const [advice, setAdvice] = useState('');
 
     // Fetch an advice from the API based on the user's input
-    const fetchQuote = async () => {
+    const fetchAdvice = async () => {
         const apiUrl = `https://api.adviceslip.com/advice/search/${encodeURIComponent(keyword || 'Love')}`;
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
-            setQuote(data["slips"][0]["advice"]); // Set the quote text
+            setAdvice(data["slips"][0]["advice"]); // Fetch the advice from the provided json
         } catch (error) { //some bit of error handling here if the API is not working or we cannot find a relevant advice. 
-            setQuote("Can't find the advice you are looking for");
+            setAdvice("Can't find the advice you are looking for");
         }
     };
 
     // What happens everytime the user submits the input
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetchQuote();
+        fetchAdvice();
     };
 
     //outputted HTML
@@ -28,18 +28,18 @@ function App() {
         <div style={{ textAlign: 'center', padding: '20px' }}>
             <h1>Advice Generator</h1>
             <form onSubmit={handleSubmit}>
-                <p>I need advice on:</p>   //the input box
+                <p>I need advice on:</p>
                 <input
                     type="text"
                     value={keyword}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={(e) => setKeyword(e.target.value)}
                     placeholder="Enter anything"
                     style={{ padding: '10px', marginBottom: '10px' }}
                 />
                
             </form>
             <div style={{ marginTop: '20px' }}>
-                {quote && <p style={{ fontSize: '1.5em', fontStyle: 'italic' }}>"{quote}"</p>}
+                {advice && <p style={{ fontSize: '1.5em', fontStyle: 'italic' }}>"{advice}"</p>}
             </div>
         </div>
     );
